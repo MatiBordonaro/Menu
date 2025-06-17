15838f506a66dbc01218ed78252319d1c4146a4e
Mati Bordonaro <matiasbordonarokpo@gmail.com>
2025-06-17 11:35:38 -0300

    agrega ruteo y about

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MenuFoodsComponent } from './menu-foods/menu-foods.component';
+import { MenuAboutComponent } from './menu-about/menu-about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'menu',
+    pathMatch: 'full'
+  },
+  {
+    path: 'menu',
+    component: MenuFoodsComponent
+  }, 
+  {
+    path: 'about',
+    component: MenuAboutComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
